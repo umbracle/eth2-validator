5b2e7940d5abe5b5c46759c41208bd50106a6ddd
Ferran Borreguero <ferranbt@protonmail.com>
2022-05-10 06:23:58 +0200

    Docker sdk (#18)
    * Introduce docker sdk
    
    * Pull image if not exists
    
    * Run containers not as host network

--- go.mod
+++ go.mod
@@ -3,21 +3,29 @@ module github.com/umbracle/eth2-validator
 go 1.16
 
 require (
+	github.com/Microsoft/go-winio v0.4.14 // indirect
 	github.com/boltdb/bolt v1.3.1
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.15+incompatible
 	github.com/ferranbt/fastssz v0.0.0-20210817104241-ab271261c6d9
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.0
 	github.com/google/gops v0.3.10
 	github.com/hashicorp/go-hclog v0.14.1
 	github.com/hashicorp/go-uuid v1.0.3
 	github.com/hashicorp/hcl v1.0.0
 	github.com/imdario/mergo v0.3.10
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/mitchellh/cli v1.1.1
 	github.com/mitchellh/mapstructure v1.3.3
-	github.com/ory/dockertest/v3 v3.6.0
+	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/runc v1.0.0-rc9 // indirect
 	github.com/r3labs/sse/v2 v2.7.4
 	github.com/stretchr/testify v1.7.0
 	github.com/supranational/blst v0.3.3
 	github.com/umbracle/ethgo v0.1.2
+	golang.org/x/time v0.0.0-20220411224347-583f2d630306 // indirect
 	google.golang.org/grpc v1.42.0
 	google.golang.org/protobuf v1.27.1
 	gopkg.in/yaml.v2 v2.3.0
--- internal/testutil/node.go
+++ internal/testutil/node.go
@@ -2,16 +2,23 @@ package testutil
 
 import (
 	"bytes"
+	"context"
 	"encoding"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
-	"github.com/ory/dockertest/v3"
-	"github.com/ory/dockertest/v3/docker"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/api/types/strslice"
+	"github.com/docker/docker/client"
+	"github.com/hashicorp/go-hclog"
 )
 
 const (
@@ -25,16 +32,25 @@ type nodeOpts struct {
 	Retry      func(n *node) error
 	Name       string
 	Mount      []string
+	InHost     bool
 	Files      map[string]string
+	Logger     hclog.Logger
 }
 
 type node struct {
-	pool     *dockertest.Pool
-	resource *dockertest.Resource
-	ip       string
+	cli  *client.Client
+	id   string
+	opts *nodeOpts
+	ip   string
 }
 
 type nodeOption func(*nodeOpts)
+
+func WithHostNetwork() nodeOption {
+	return func(n *nodeOpts) {
+		n.InHost = true
+	}
+}
 
 func WithContainer(repository, tag string) nodeOption {
 	return func(n *nodeOpts) {
@@ -90,24 +106,21 @@ func WithFile(path string, obj interface{}) nodeOption {
 
 func newNode(opts ...nodeOption) (*node, error) {
 	nOpts := &nodeOpts{
-		Mount: []string{},
-		Files: map[string]string{},
-		Cmd:   []string{},
+		Mount:  []string{},
+		Files:  map[string]string{},
+		Cmd:    []string{},
+		Logger: hclog.L(),
+		InHost: false,
 	}
 	for _, opt := range opts {
 		opt(nOpts)
 	}
 
-	pool, err := dockertest.NewPool("")
+	ctx := context.Background()
+
+	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		return nil, fmt.Errorf("could not connect to docker: %s", err)
-	}
-
-	dockerOpts := &dockertest.RunOptions{
-		Repository: nOpts.Repository,
-		Tag:        nOpts.Tag,
-		Cmd:        nOpts.Cmd,
-		Mounts:     []string{},
 	}
 
 	// setup configuration
@@ -124,7 +137,6 @@ func newNode(opts ...nodeOption) (*node, error) {
 			return nil, err
 		}
 		mountMap[mount] = tmpDir
-		dockerOpts.Mounts = append(dockerOpts.Mounts, tmpDir+":"+mount)
 	}
 
 	// build the files
@@ -157,18 +169,65 @@ func newNode(opts ...nodeOption) (*node, error) {
 		}
 	}
 
-	resource, err := pool.RunWithOptions(dockerOpts)
+	imageName := nOpts.Repository + ":" + nOpts.Tag
+
+	// pull image if it does not exists
+	_, _, err = cli.ImageInspectWithRaw(ctx, imageName)
 	if err != nil {
-		return nil, fmt.Errorf("could not start node: %s", err)
+		reader, err := cli.ImagePull(ctx, imageName, types.ImagePullOptions{})
+		if err != nil {
+			return nil, err
+		}
+		_, err = io.Copy(nOpts.Logger.StandardWriter(&hclog.StandardLoggerOptions{}), reader)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	config := &container.Config{
+		Image: imageName,
+		Cmd:   strslice.StrSlice(nOpts.Cmd),
+	}
+	hostConfig := &container.HostConfig{
+		Binds: []string{},
+	}
+	if nOpts.InHost {
+		hostConfig.NetworkMode = "host"
+	}
+
+	for mount, local := range mountMap {
+		hostConfig.Binds = append(hostConfig.Binds, local+":"+mount)
+	}
+
+	body, err := cli.ContainerCreate(ctx, config, hostConfig, &network.NetworkingConfig{}, nil, "")
+	if err != nil {
+		return nil, fmt.Errorf("could not create container: %v", err)
+	}
+	containerID := body.ID
+
+	// start container
+	if err := cli.ContainerStart(ctx, containerID, types.ContainerStartOptions{}); err != nil {
+		return nil, fmt.Errorf("could not start container: %v", err)
 	}
 
 	n := &node{
-		pool:     pool,
-		resource: resource,
-		ip:       resource.Container.NetworkSettings.IPAddress,
+		cli:  cli,
+		id:   containerID,
+		opts: nOpts,
+		ip:   "127.0.0.1",
 	}
+
+	// get the ip of the node if not running as a host network
+	if !nOpts.InHost {
+		containerData, err := cli.ContainerInspect(ctx, containerID)
+		if err != nil {
+			return nil, err
+		}
+		n.ip = containerData.NetworkSettings.IPAddress
+	}
+
 	if nOpts.Retry != nil {
-		if err := pool.Retry(func() error {
+		if err := retryFn(func() error {
 			return nOpts.Retry(n)
 		}); err != nil {
 			return nil, err
@@ -179,28 +238,41 @@ func newNode(opts ...nodeOption) (*node, error) {
 }
 
 func (n *node) Stop() {
-	n.pool.Purge(n.resource)
+	if err := n.cli.ContainerStop(context.Background(), n.id, nil); err != nil {
+		n.opts.Logger.Error("failed to stop container", "id", n.id, "err", err)
+	}
 }
 
 func (n *node) GetLogs() (string, error) {
 	wr := bytes.NewBuffer(nil)
 
-	logsOptions := docker.LogsOptions{
-		Container:    n.resource.Container.ID,
-		OutputStream: wr,
-		ErrorStream:  wr,
-		Follow:       false,
-		Stdout:       true,
-		Stderr:       true,
+	out, err := n.cli.ContainerLogs(context.Background(), n.id, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
+	if err != nil {
+		return "", nil
 	}
-	if err := n.pool.Client.Logs(logsOptions); err != nil {
+	if _, err := io.Copy(wr, out); err != nil {
 		return "", err
 	}
-
 	logs := wr.String()
 	return logs, nil
 }
 
 func (n *node) IP() string {
 	return n.ip
+}
+
+func retryFn(handler func() error) error {
+	timeoutT := time.NewTimer(1 * time.Minute)
+
+	for {
+		select {
+		case <-time.After(5 * time.Second):
+			if err := handler(); err == nil {
+				return nil
+			}
+
+		case <-timeoutT.C:
+			return fmt.Errorf("timeout")
+		}
+	}
 }
